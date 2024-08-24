ceb70abe8ef4c703b0db2b98936a395a7b349b97
SupavithLOL <supavithgithub@gmail.com>
2024-08-24 12:00:54 +0700

    Practice2 Weather API August-24-2024

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
-import FlateListExample from './components/FlateListExample';
-import FlatListcallBackend from './components/FlatListcallBackend';
-import NewsApp from './components/NewsApp';
+import React from "react";
+import { StyleSheet, Text, View, Button, Alert, TextInput } from "react-native";
+import NewsApp from "./components/NewsApp";
+import AxiosgetData from "./components/AxiosgetData";
+import AxiosPostData from "./components/AxiosPostData";
+import WeatherLondon from "./components/WeatherLondon";
+import WeatherBangkok from "./components/WeatherBangkok";
 
-export default function App() :React.JSX.Element {
-
+export default function App(): React.JSX.Element {
   return (
     <View>
-      {/* <FlateListExample/> */}
-      {/* <FlatListcallBackend/> */}
-      <NewsApp />
+      {/* <AxiosgetData /> */}
+      {/* <AxiosPostData/> */}
+      {/* <WeatherLondon/> */}
+      <WeatherBangkok/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+});
